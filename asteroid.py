8459023bbca34cb8620e98432f4bd668c091f83e
Maik-Schulze <maik.schulze99@gmail.com>
2025-01-14 14:44:05 +0100

    Add asteroid splitting functionality and update collision handling

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,23 @@ class Asteroid(CircleShape):
     def update(self, dt):
         # Update the position of the asteroid based on its velocity and the time delta
         self.position += self.velocity * dt
+    
+    def split(self):
+        # Remove the current asteroid
+        self.kill()
+        
+        # If the asteroid is too small, do not split
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        # Calculate the angle for splitting and the radius of the new asteroids
+        angle = random.uniform(20, 50)
+        radius = self.radius - ASTEROID_MIN_RADIUS
+
+        # Create the left asteroid with adjusted velocity
+        left_asteroid = Asteroid(self.position[0], self.position[1], radius)
+        left_asteroid.velocity = self.velocity.rotate(-angle) * 1.2
+
+        # Create the right asteroid with adjusted velocity
+        right_asteroid = Asteroid(self.position[0], self.position[1], radius)
+        right_asteroid.velocity = self.velocity.rotate(angle) * 1.2
--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
 			for shot in shots:
 				if asteroid.collides_with(shot):
 					shot.kill()
-					asteroid.kill()
+					asteroid.split()
 
 		# Fill the screen with black color
 		pygame.Surface.fill(screen, (0, 0, 0))

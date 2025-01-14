1c55c64426b0501872ea5a2baa2c52967530f139
Maik-Schulze <maik.schulze99@gmail.com>
2025-01-14 13:23:01 +0100

    Add frame rate control and improve game loop structure

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ import pygame
 from constants import *
 
 def main():
+	# Initialize the pygame module
 	pygame.init()
-	print("Starting asteroids!")
+
+	# Create a clock object to manage the frame rate
+	clock = pygame.time.Clock()
+	dt = 0
+
+	# Set up the display window with the specified width and height
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	while True:
+	
+	while True: #game loop
+		# Handle events in the event queue
 		for event in pygame.event.get():
-		    if event.type == pygame.QUIT:
-        		return
-
-		pygame.Surface.fill(screen, (0,0,0))
+			# Exit the game loop if the quit event is triggered
+			if event.type == pygame.QUIT:
+				return
+		
+		# Fill the screen with black color
+		pygame.Surface.fill(screen, (0, 0, 0))
+		# Update the full display surface to the screen
 		pygame.display.flip()
+		
+		# Cap the frame rate and calculate the time delta
+		dt = clock.tick(MAX_FPS) / 1000
 
-
+# Run the main function if this script is executed
 if __name__ == "__main__":
 	main()
